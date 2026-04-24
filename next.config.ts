16ac8a8908638aa60a5aa39ab9d30c2efc824af0
Ayush <ayushkumar85385@gmail.com>
2026-04-24 18:25:03 +0530

    recrawl

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/debateTimer",
+        destination: "/debate-timer",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
